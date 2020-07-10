d6164a3bade1dad22e6848a26493aeff609d9845
Sandkelp <54632799+Sandkelp@users.noreply.github.com>
2020-07-10 15:42:36 -0400

    Update github_commit_log.py

--- gitlog/scripts/github_commit_log.py
+++ gitlog/scripts/github_commit_log.py
@@ -2,45 +2,39 @@ import json
 import requests
 from pprint import pprint
 from time import sleep
+from datetime import datetime
+from time_rn import right_time # imports the time function
 
 class Get_Github_Info:
     def __init__(self):
-        self.username = ""#username here
-        self.repository = ""#reponame here
-        print("Public events for the repository {} in {}: \n".format(self.repository,self.username))
-        response = requests.get('https://api.github.com/repos/{}/{}/commits'.format(self.username,self.repository)).json()
-        pprint(response)
-    def get_updated_file(self):
-        new_file = requests.get('https://api.github.com/repos/{}/{}/commits'.format(self.username,self.repository)).json()
-        return len(new_file), new_file
+        self.username = "Sandkelp" # sets the username
+        self.repository = "create" #set the repo name
+        
+    def first_print(self):
+        time_now = right_time() #gets the time 
+        print("Public events for the repository {} in {} since {}: \n".format(self.repository,self.username,time_now) )# title
+        response = requests.get('https://api.github.com/repos/{}/{}/commits?since={}Z'.format(self.username,self.repository,time_now)).json() #gets the commits from the start of the code
+        pprint(response) #prints them
+    
+    def wait_function(self):
+        time_now = right_time() #gets the time
+        sleep(300) #waits 5 mins
+        return time_now #returns the time before 5 mins
+
+    def get_updated_file(self, time):
+        new_file = requests.get('https://api.github.com/repos/{}/{}/commits?since={}Z'.format(self.username,self.repository,time)).json()# gets all of the commits within the last 5 mins
+        pprint(new_file) #prints out the commits within the last 5 mins
+        #return len(new_file), new_file
         
     
 
     
     
         
-    def compare_files(self, file1, file2):
-        sleep(5)
-        if file1 == file2:
-            return True
-        else:
-            return False
+    
 
 
-
-
-g = Get_Github_Info()
+g = Get_Github_Info()# starts the class
+g.first_print() #calls the first list of commits
 while 1==1:
-    status1, content1 = g.get_updated_file()
-    sleep(5) 
-    status2, content2 = g.get_updated_file()
-    status = g.compare_files(status1, status2)
-    if status == True:
-        pass
-    elif status == False:
-        pprint(content2[status1:status2])
-
-
-
-
-
+    g.get_updated_file(g.wait_function()) #every 5 mins the commits are printed
