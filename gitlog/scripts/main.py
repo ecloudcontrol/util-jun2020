8e04cd6866931837c7c8bfa502ad6963cd008549
Sandkelp <54632799+Sandkelp@users.noreply.github.com>
2020-07-13 17:54:24 -0400

    Update main.py

--- gitlog/scripts/main.py
+++ gitlog/scripts/main.py
@@ -9,32 +9,20 @@ class Get_Github_Info:
     def __init__(self):
         self.username = "Sandkelp" # sets the username
         self.repository = "create" #set the repo name
-        
-    def first_print(self):
-        time_now = right_time() #gets the time 
-        print("Public events for the repository {} in {} since {}: \n".format(self.repository,self.username,time_now) )# title
-        response = requests.get('https://api.github.com/repos/{}/{}/commits?since={}Z'.format(self.username,self.repository,time_now)).json() #gets the commits from the start of the code
-        pprint(response) #prints them
     
     def wait_function(self):
         time_now = right_time() #gets the time
-        sleep(300) #waits 5 mins
+        sleep(30) #waits 5 mins
         return time_now #returns the time before 5 mins
 
     def get_updated_file(self, time):
-        new_file = requests.get('https://api.github.com/repos/{}/{}/commits?since={}Z'.format(self.username,self.repository,time)).json()# gets all of the commits within the last 5 mins
-        pprint(new_file) #prints out the commits within the last 5 mins
-        #return len(new_file), new_file
+        response = requests.get('https://api.github.com/repos/{}/{}/commits?since={}Z'.format(self.username,self.repository,time)).json()# gets all of the commits within the last 5 mins
+        new_file = response[0]
+        print('name: ' + str(new_file['commit']['author']['name']))#prints name
+        print('comment_count: ' + str(new_file['commit']['comment_count']))# prints comment count
+        print('message: ' +str(new_file['commit']['message']))# prints the message
         
-    
-
-    
-    
-        
-    
-
 
 g = Get_Github_Info()# starts the class
-g.first_print() #calls the first list of commits
 while 1==1:
-    g.get_updated_file(g.wait_function()) #every 5 mins the commits are printed
+    g.get_updated_file(g.wait_function()) #every 5 mins gets updates
