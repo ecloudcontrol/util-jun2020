50bf0a7b7b1d1909502947878d9e883b8f725fc0
Sandkelp <54632799+Sandkelp@users.noreply.github.com>
2020-07-14 17:06:07 -0400

    Update time_rn.py

--- gitlog/scripts/time_rn.py
+++ gitlog/scripts/time_rn.py
@@ -5,3 +5,4 @@ def right_time():
     now_utc = datetime.now(timezone('UTC')) #gets the current time
     now_pacific = now_utc.astimezone(timezone('Etc/GMT0')) # sets the time 4 hours ahead for github to return what was just commited
     return now_pacific.strftime(fmt) $ #sets the time for the github api to use 
+d
