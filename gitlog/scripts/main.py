431ad8aad3da753a2d44fda479406a9d9ea0d726
Sandkelp <54632799+Sandkelp@users.noreply.github.com>
2020-07-17 16:42:31 -0400

    Update main.py

--- gitlog/scripts/main.py
+++ gitlog/scripts/main.py
@@ -1,5 +1,5 @@
 import json 
-import requests 
+import requests
 from pprint import pprint
 from time import sleep
 from datetime import datetime
@@ -9,32 +9,47 @@ class Get_Github_Info:
     def __init__(self):
         self.username = "kubernetes" # sets the username
         self.repository = "kubernetes" #set the repo name
-        self.ref = "refs/heads/20200702/j2y"
+        
     
     def wait_function(self):
         time_now = right_time() #gets the time
-        sleep(300) #waits 5 mins
+        sleep(302) #waits 5 mins
         return time_now#returns the time before 5 mins
 
-    def get_updated_file(self, username, repository,time):
-        response = requests.get('https://api.github.com/repos/{}/{}/commits?since={}Z'.format(username,repository,time)).json()# gets all of the commits within the last 5 mins
-        num_files = requests.get('https://api.github.com/repos/{}/{}/commits/master'.format(username,repository)).json()
-        if len(response) == 0:
-            pass
-        else:
-            for i in range(len(response)):
-                print(username)
-                new_file = response[i]
-                json_commit = {'commit':[{'username':username,
-                'repo_name':repository,
-                'date':new_file['commit']['author']['date'],
-                'email':new_file['commit']['author']['name'],
-                'name':new_file['commit']['author']['name'],
-                'comment_count':new_file['commit']['comment_count'],
-                'message':new_file['commit']['message'],
-                'files_changed':len(num_files['files'])}]}
-                print(json_commit)
+    def get_updated_file(self, username, repository,time):    
+        new_dates = []
+        response = requests.get('https://api.github.com/repos/{}/{}/events'.format(username,repository)).json() # gets all of the commits within the last 5 mins
+        start_date = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S")
         
+        for i in range(len(response)):
+            
+            file_ = response[i]
+            if file_['type'] == "PushEvent":
+                file_times = file_['created_at']
+                current_date = datetime.strptime(file_times[:-1], "%Y-%m-%dT%H:%M:%S")
+                
+                if current_date > start_date:
+                    new_dates.append(i)
+            else:
+                pass
+        print(new_dates)
+        for i in range(len(new_dates)):
+            try: 
+                commit_info = requests.get(response[i]['payload']['commits'][0]['url']).json()       
+            
+                for i in range(len(new_dates)):   
+                    print(username)
+                    json_commit = {'commit':[{'username':username,
+                    'repo_name':repository,
+                    'date':commit_info['commit']['author']['date'],
+                    'email':commit_info['commit']['author']['name'],
+                    'name':commit_info['commit']['author']['name'],
+                    'comment_count':commit_info['commit']['comment_count'],
+                    'message':commit_info['commit']['message'],
+                    'files_changed':len(commit_info['files'])}]}
+                    print(json_commit)
+            except KeyError:
+                pass
 repos = {
      1: {
         "user_name" : "freeCodeCamp",
@@ -45,8 +60,8 @@ repos = {
         "repo_name" : "996.ICU"
     },
      3: {
-        "user_name" : "vuejs",
-        "repo_name" : "vue"
+        "user_name" : "Sandkelp",
+        "repo_name" : "create"
     },
      4: {
         "user_name" : "EbookFoundation",
@@ -73,8 +88,8 @@ repos = {
         "repo_name" : "papers-we-love"
     },
      0: {
-        "user_name" : "TowhidKashem",
-        "repo_name" : "snapchat-clone"
+        "user_name" : "EcloudControl",
+        "repo_name" : "util-jun2020"
     }
 }
 g = Get_Github_Info()# starts the class
